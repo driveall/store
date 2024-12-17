0d1a7221c483320519a67e7f94ee7e7b65aafd5a
Oleksii Reznik <oleksii.reznik@yalantis.net>
2024-12-17 23:23:02 +0200

    refactor controllers to separate crud and view

--- src/main/java/com/daw/store/controller/StoreController.java
+++ src/main/java/com/daw/store/controller/StoreController.java
@@ -1,16 +1,11 @@
 package com.daw.store.controller;
 
+import com.daw.store.entity.AccountEntity;
 import com.daw.store.service.AccountService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-
-import static com.daw.store.Constants.ATTRIBUTE_LOGIN;
-
-@RestController
+@RestController()
 @Slf4j
 public class StoreController {
 
@@ -20,85 +15,33 @@ public class StoreController {
         this.accountService = accountService;
     }
 
-    @PostMapping("/login")
-    public void login(@RequestParam String login,
-                      @RequestParam String pass,
-                      HttpServletRequest req,
-                      HttpServletResponse resp) throws IOException {
-        log.info("login for " + login);
-        if (accountService.accountExists(login) && accountService.login(login, pass)) {
-            req.getSession().setAttribute(ATTRIBUTE_LOGIN, login);
-            resp.sendRedirect("/success");
-        } else {
-            resp.sendRedirect("/index.html");
-        }
+    @GetMapping("/api/get")
+    public AccountEntity get(@RequestParam String login) {
+        log.info("api get for " + login);
+        var account = new AccountEntity();
+        account.setLogin(login);
+        return accountService.getByLogin(account.getLogin());
     }
 
-    @PostMapping("/register")
-    public void register(@RequestParam String login,
-                         @RequestParam String pass,
-                         @RequestParam String pass2,
-                         HttpServletRequest req,
-                         HttpServletResponse resp) throws IOException {
-        log.info("register for " + login);
-        if (accountService.passwordConfirmed(pass, pass2)
+    @PostMapping("/api/create")
+    public AccountEntity create(@RequestBody AccountEntity account) {
+        log.info("api register for " + account.getLogin());
+        var login = account.getLogin();
+        if (accountService.passwordVerification(account.getPassword(), account.getPasswordConfirmed())
                 && !accountService.accountExists(login)) {
-            accountService.createAccount(login, pass);
-            req.getSession().setAttribute(ATTRIBUTE_LOGIN, login);
-            resp.sendRedirect("/success");
+            accountService.createAccount(account);
+            return accountService.getByLogin(account.getLogin());
         } else {
-            resp.sendRedirect("/index.html");
-        }
-    }
-
-    @PostMapping("/unlogin")
-    public void unlogin(HttpServletRequest req,
-                        HttpServletResponse resp) throws IOException {
-        log.info("unlogin for " + req.getSession().getAttribute(ATTRIBUTE_LOGIN));
-        req.getSession().removeAttribute(ATTRIBUTE_LOGIN);
-        resp.sendRedirect("/index.html");
-    }
-
-    @GetMapping("/success")
-    public String success(HttpServletRequest req,
-                        HttpServletResponse resp) throws IOException {
-        var login = (String) req.getSession().getAttribute(ATTRIBUTE_LOGIN);
-        log.info("success for " + login);
-        if (login != null) {
-            return String.format("""
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                        <meta charset="UTF-8">
-                        <link rel="stylesheet" href="styles.css">
-                    </head>
-                    <body>
-                    <div class="center470">
-                    <h1>Login Successful for %s</h1>
-                    <form action="/delete" method="post">
-                        <input type="hidden" name="%s" value="%s">
-                        <input type="submit" value="Delete" class="btn200" >
-                    </form>
-                    <form action="/unlogin" method="post">
-                        <input type="submit" value="Unlogin" class="btn200" >
-                    </form>
-                    </div>
-                    </body>
-                    </html>
-                    """, login, ATTRIBUTE_LOGIN, login);
-        } else {
-            resp.sendRedirect("/index.html");
             return null;
         }
     }
 
-    @PostMapping("/delete")
-    public void delete(@RequestParam String login,
-                       HttpServletRequest req,
-                       HttpServletResponse resp) throws IOException {
-        log.info("delete for " + login);
-        req.getSession().removeAttribute(ATTRIBUTE_LOGIN);
-        accountService.deleteAccount(login);
-        resp.sendRedirect("/index.html");
+    @DeleteMapping("/api/delete")
+    public int delete(@RequestParam String login) {
+        log.info("api delete for " + login);
+        var account = new AccountEntity();
+        account.setLogin(login);
+        accountService.deleteAccount(account);
+        return 200;
     }
 }
---
+++ src/main/java/com/daw/store/controller/ViewController.java
@@ -0,0 +1,139 @@
+package com.daw.store.controller;
+
+import com.daw.store.entity.AccountEntity;
+import com.daw.store.service.AccountService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static com.daw.store.Constants.*;
+import static com.daw.store.Constants.ATTRIBUTE_LOGIN;
+
+@RestController
+@Slf4j
+public class ViewController {
+    private final AccountService accountService;
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public ViewController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/login")
+    public void login(@RequestParam String login,
+                      @RequestParam String pass,
+                      HttpServletRequest req,
+                      HttpServletResponse resp) {
+        log.info("login for " + login);
+
+        var accountEntity = new AccountEntity();
+        accountEntity.setLogin(login);
+        accountEntity.setPassword(pass);
+
+        var params = new HashMap<String, String>();
+        params.put(ATTRIBUTE_LOGIN, login);
+
+        var response = restTemplate.getForEntity(String.format(API_GET_URL, login), AccountEntity.class);
+
+        if (response.getBody() != null) {
+            accountEntity = response.getBody();
+            if (accountService.login(accountEntity)) {
+                addSessionAttribute(req, response.getBody().getLogin());
+                redirect(resp, SUCCESS_PAGE_PATH);
+            } else {
+                redirect(resp, INDEX_PAGE_PATH);
+            }
+        } else {
+            redirect(resp, INDEX_PAGE_PATH);
+        }
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestParam String login,
+                         @RequestParam String pass,
+                         @RequestParam String pass2,
+                         HttpServletRequest req,
+                         HttpServletResponse resp) {
+        log.info("register for " + login);
+        if (accountService.passwordVerification(pass, pass2)
+                && !accountService.accountExists(login)) {
+            var accountEntity = new AccountEntity();
+            accountEntity.setLogin(login);
+            accountEntity.setPassword(pass);
+            accountEntity.setPasswordConfirmed(pass2);
+
+            var response = restTemplate.postForEntity(API_CREATE_URL, accountEntity, AccountEntity.class);
+
+            if (response.getBody() != null) {
+                addSessionAttribute(req, response.getBody().getLogin());
+                redirect(resp, SUCCESS_PAGE_PATH);
+            } else {
+                redirect(resp, INDEX_PAGE_PATH);
+            }
+        } else {
+            redirect(resp, INDEX_PAGE_PATH);
+        }
+    }
+
+    @PostMapping("/unlogin")
+    public void unlogin(HttpServletRequest req,
+                        HttpServletResponse resp) {
+        log.info("unlogin for " + req.getSession().getAttribute(ATTRIBUTE_LOGIN));
+        removeSessionAttribute(req);
+        redirect(resp, INDEX_PAGE_PATH);
+    }
+
+    @GetMapping("/success")
+    public String success(HttpServletRequest req,
+                          HttpServletResponse resp) {
+        var login = getSessionAttribute(req);
+        log.info("success for " + login);
+        if (login != null) {
+            return String.format(SUCCESS_PAGE_CONTENT, login, ATTRIBUTE_LOGIN, login);
+        } else {
+            redirect(resp, INDEX_PAGE_PATH);
+            return null;
+        }
+    }
+
+    @PostMapping("/delete")
+    public void delete(@RequestParam String login,
+                       HttpServletRequest req,
+                       HttpServletResponse resp) {
+        log.info("delete for " + login);
+        removeSessionAttribute(req);
+
+        var accountEntity = new AccountEntity();
+        accountEntity.setLogin(login);
+
+        restTemplate.delete(String.format(API_DELETE_URL, login), accountEntity);
+
+        redirect(resp, INDEX_PAGE_PATH);
+    }
+
+    private void addSessionAttribute(HttpServletRequest req, String login) {
+        req.getSession().setAttribute(ATTRIBUTE_LOGIN, login);
+    }
+    private void removeSessionAttribute(HttpServletRequest req) {
+        req.getSession().removeAttribute(ATTRIBUTE_LOGIN);
+    }
+    private String getSessionAttribute(HttpServletRequest req) {
+        return (String) req.getSession().getAttribute(ATTRIBUTE_LOGIN);
+    }
+
+    private void redirect(HttpServletResponse resp, String path) {
+        try {
+            resp.sendRedirect(path);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
