4a77734c91a2bb219064114f2c1489da092adf66
Oleksii Reznik <oleksii.reznik@yalantis.net>
2024-12-20 21:56:41 +0200

    remove view resolver

--- src/main/java/com/daw/store/StoreApplication.java
+++ src/main/java/com/daw/store/StoreApplication.java
@@ -4,23 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
-    }
-
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        var viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
     }
 
     @Bean
